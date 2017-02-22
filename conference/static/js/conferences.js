51017c995364227c158d8ecb1ad8534c71eaf766
Sylvain Boily <sylvainboiydroid@gmail.com>
2017-02-22 11:49:11 -0500

    use no-sort class and remove obsolete js

--- conference/static/js/conferences.js
+++ conference/static/js/conferences.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-    $('#table-conferences').DataTable({
-        'lengthChange': false,
-        'searching': false
-    });
-
     $('#add-conference').click(function() {
         $('#view-add-conference').removeClass('hidden').removeAttr('style');
     });
