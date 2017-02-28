f29b604c6c89e43188a80615ece9893133cb8b16
Francois Blackburn <blackburnfrancois@gmail.com>
2017-02-28 14:37:08 -0500

    use create_blueprint helper

--- conference/plugin.py
+++ conference/plugin.py
@@ -2,15 +2,14 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-from flask import Blueprint
 from flask_menu.classy import register_flaskview
 
+from wazo_admin_ui.helpers.plugin import create_blueprint
 
 from .service import ConferenceService
 from .view import ConferenceView
 
-conference = Blueprint('conference', __name__, template_folder='templates',
-                       static_folder='static', static_url_path='/%s' % __name__)
+conference = create_blueprint('conference', __name__)
 
 
 class Plugin(object):
